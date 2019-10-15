dfcfd2d18fe4d5af2c2d0e985f80d4b7d0523933
Riley Flynn <technologyflynnteam@gmail.com>
2019-10-15 21:17:58 -0230

    Added experimental gm base

--- handler.ts
+++ handler.ts
@@ -1,18 +1,23 @@
 import { APIGatewayEvent } from "aws-lambda";
+import { getImage, getImageSize } from "./src/utils";
 
 export async function hello(event: APIGatewayEvent): Promise<any> {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v1.0! Your function executed successfully!",
-        input: event
-      },
-      null,
-      2
-    )
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  if (event.queryStringParameters && event.queryStringParameters.url) {
+    const image = await getImage(event.queryStringParameters.url);
+    const imageSize = await getImageSize(image);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        width: imageSize.width,
+        height: imageSize.height
+      })
+    };
+  } else {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: 'Please specify a "url" query parameter'
+      })
+    };
+  }
 }
---
+++ src/utils.ts
@@ -0,0 +1,18 @@
+import gm, { State, Dimensions } from "gm";
+import request from "request-promise-native";
+
+export async function getImage(url: string): Promise<State> {
+  return gm.subClass({ imageMagick: true })(await request(url));
+}
+
+export function getImageSize(image: State): Promise<Dimensions> {
+  return new Promise((resolve, reject) => {
+    image.size((err, value) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(value);
+      }
+    });
+  });
+}
