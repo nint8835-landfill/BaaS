dc43be36d736a141f05118f2c91294fb7ced2f69
Riley Flynn <riley@colabsoftware.com>
2019-10-17 18:46:51 -0230

    Made everything square

--- src/operations/minweld.ts
+++ src/operations/minweld.ts
@@ -2,8 +2,9 @@ import { State } from "gm";
 
 export const minweld = async (image: State): Promise<State> =>
   image
-    .resize(800, 800, ">")
+    .resize(800, 800, "!")
     .blackThreshold(0.5)
     .sharpen(2, 5)
     .implode(0.6)
-    .wave(10, 100);
+    .wave(10, 100)
+    .crop(400, 400);
