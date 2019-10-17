dd38079a636a893f817aaab8372e0f8fb569e1aa
Riley Flynn <riley@colabsoftware.com>
2019-10-17 18:29:01 -0230

    Changed minweld operations

--- src/operations/minweld.ts
+++ src/operations/minweld.ts
@@ -3,7 +3,6 @@ import { State } from "gm";
 export const minweld = async (image: State): Promise<State> =>
   image
     .resize(800, 800, ">")
-    .blackThreshold(0.9, 0, 0)
-    .sharpen(0)
-    .implode(0.2)
-    .quality(8);
+    .blackThreshold(0.5)
+    .sharpen(2)
+    .implode(2);
