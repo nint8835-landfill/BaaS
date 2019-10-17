a4ce0f660450a549abe628d3302a10384d961b38
Riley Flynn <riley@colabsoftware.com>
2019-10-17 18:57:27 -0230

    Remove crop

--- src/operations/minweld.ts
+++ src/operations/minweld.ts
@@ -6,5 +6,4 @@ export const minweld = async (image: State): Promise<State> =>
     .blackThreshold(0.5)
     .sharpen(2, 5)
     .implode(0.6)
-    .wave(10, 100)
-    .crop(700, 700, 350, 350);
+    .wave(10, 100);
