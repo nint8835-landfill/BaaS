036c35cd5e7848f21382f0f4dcef5bf51cefb6c3
Riley Flynn <technologyflynnteam@gmail.com>
2019-10-16 21:35:01 -0230

    Clean weld

--- src/operations/minweld.ts
+++ src/operations/minweld.ts
@@ -1,10 +1,9 @@
 import { State } from "gm";
 
-export async function minweld(image: State): Promise<State> {
-  return image
+export const minweld = async (image: State): Promise<State> =>
+  image
     .resize(800, 800, ">")
     .blackThreshold(0.9, 0, 0)
     .sharpen(0)
     .implode(0.2)
     .quality(8);
-}
